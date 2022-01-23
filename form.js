6795d8126a6a239b72947d04ed1c13a958697b8f
Jyothi Lakshmi <jyothilakshmi5221@gmail.com>
2022-01-23 14:01:55 +0530

    NEWGIT

--- form.js
+++ form.js
@@ -1,9 +1,12 @@
-let email=document.getElementById("email");
-let error=document.getElementById("error");
-let phoneno=document.getElementById("phoneno");
-    let password=document.getElementById("password");
+ let email=document.getElementById("email");
+ let error=document.getElementById("error");
+ let phoneno=document.getElementById("phoneno");
+let password = document.getElementById("password");
    
-
+// if((email.trim()=="") && (password.trim()=="")){
+//   alert("Feilds cannot be blank");
+//   return false;
+// }
 
     
 
@@ -16,7 +19,7 @@ let phoneno=document.getElementById("phoneno");
              error.style.color = "green";
        //    // alert("valid")
              return true;
-   
+             
         }
          else {
               error.innerHTML = "Invalid :Phone No should be in the format XXXXXXXXXX";
@@ -31,24 +34,30 @@ let phoneno=document.getElementById("phoneno");
         
          
     
-     
-    function valid() {
-        let RegExp=/^([A-Za-z+0-9?])$/;
-         if(RegExp.test(password.value)){}
+        
+    function valid(){
+      let password = document.getElementById("password");
+         let RegExp=/^([A-Za-z0-9])$/;
+           if(RegExp.test(password.value)){};
           if(password.value.length<8){
-          
+          error.innerHTML = "Password is too weak";
+          error.style.color = "red";
          alert("Password is too weak");
          password.style.border = "2px solid red";
          return false;
      }
       if(password.value.length<=8){
         alert("Password is medium");
+        error.innerHTML = "Password is medium";
+        error.style.color = "orange";
         password.style.border = "2px solid orange";
          return false;
      }
     else if(password.value.length>8){
        
         alert("Password is strong");
+        error.innerHTML = "Password is stroong";
+        error.style.color = "green";
         password.style.border = "2px solid green";
         return true;
      }
@@ -63,12 +72,17 @@ let phoneno=document.getElementById("phoneno");
                return true;
            }
            else{
-             alert("Email should be in the correct format.");
+             alert("Email should be eg@gmail.xxx format.");
                error.innerHTML = "Invalid";
                error.style.color = "red";
                error.style.border = "2px soild red";
                return false;
            }
        }
-
+ function submitter(){
+   if (!validate())return false;
+   if (!valid())return false;
+   if (!validation())return false;
+   return true;
+ }
     
